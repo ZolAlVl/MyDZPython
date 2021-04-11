47daf659ebc9b4edbee4ee782d474ab41572e606
ZolAlVl <82275731+ZolAlVl@users.noreply.github.com>
2021-04-11 11:38:32 +0300

    Update DZ3.py

--- DZ3.py
+++ DZ3.py
@@ -3,11 +3,11 @@ result = value / 2 if value < 100 else -value
 print (result)
 ############################################################
 value = 107
-result = "1" if value < 100 else "0"
+result = 1 if value < 100 else 0
 print(result)
 # ##########################################################
 value = 120
-result = "True" if value < 100 else "False"
+result = True if value < 100 else False
 print(result)
 #########################################################
 my_str = "Hello, World!"
